354a73435dedf0f20dc02be4d7e4346784d98704
LingZhouChen <lingzhouc@gmail.com>
2023-10-23 15:02:04 -0400

    complete lab

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,31 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
+
+greet("Sam")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("Sam")
 
 def add(num1, num2):
     pass
+    return num1 + num2
+
+add(1, 2)
 
 def halve(number):
     pass
+    return number / 2
+
+halve(4)
